e7de0a89414753ac53fd37270af0bf30cd15a1d5
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-02-25 00:19:00 +0530

    contactus linked with db

--- ContactUs/contactusback.php
+++ ContactUs/contactusback.php
@@ -1,22 +1,24 @@
 <?php
 
 require_once('../Authentication/Db_connection.php');
-if(isset($_POST["Send"])) {
+if($_SERVER['REQUEST_METHOD']== 'POST') {
     $name=$_POST["name"];
     $email=$_POST["email"];
     $phone=$_POST["phone"];
     $message=$_POST["message"];
+    // insert data into db
     $sql="INSERT INTO contactus(name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
-    $result=mysqli_query($con,$sql);
+    $result=mysqli_query($conn,$sql);
 
-    if(mysqli_query($con,$sql)){
-        echo "Details successfully added ";
+    if($result){
+       // echo "Details successfully added ";
         header("Location:../ContactUS/index.php");
+        exit(); // prevents further execution
     }else{
-        echo "record not saved";
+        echo "record not saved" . mysqli_error($conn);
     }
 } 
 
-
+mysqli_close($conn);
 
 ?>
